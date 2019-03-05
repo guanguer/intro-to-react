f47b43bfd3bbd6b4a6eafd698732e5c1af087ab3
Juan Fernando Taborda Sanchez <juan.taborda@globant.com>
2019-03-04 23:35:07 -0500

    initial commit

---
+++ src/Details.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { petfinder } from "./config";
+import Carousel from "./Carousel";
+import Modal from "./Modal";
+
+class Details extends React.Component {
+  state = {
+    loading: true,
+    showModal: false
+  };
+
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
+  componentDidMount() {
+    petfinder.pet
+      .get({
+        output: "full",
+        id: this.props.id
+      })
+      .then(data => {
+        const pet = data.petfinder.pet;
+        let breed;
+        if (Array.isArray(data.petfinder.pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        } else {
+          breed = pet.breeds.breed;
+        }
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: err
+        });
+      });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
+    }
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      media,
+      showModal
+    } = this.state;
+    return (
+      <div className="details">
+        <Carousel media={media} />
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+          <button onClick={this.toggleModal}>Adopt {name}</button>
+          <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <h1>Would you like to adopt {name}</h1>
+              <div className="buttons">
+                <button onClick={this.toggleModal}>Yes</button>
+                <button onClick={this.toggleModal}>Definitely Yes!</button>
+              </div>
+            </Modal>
+          ) : null}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Details;
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+const config = {
+  context: __dirname,
+  entry: ["./src/App.js"],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/"
+  },
+  devtool: "cheap-eval-source-map",
+  devServer: {
+    historyApiFallback: true,
+    port: 9000
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"]
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.jsx?$/,
+        loader: "eslint-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(js|jsx)?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: "body",
+      template: "./src/index.html"
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: "src",
+        ignore: ["*.jsx", "*.js", "*.css"]
+      }
+    ]),
+    new Dotenv()
+  ]
+};
+
+module.exports = config;
