db0995c77a99108698ef0ed397aaaf16a4e164dd
Juan Taborda <jftabord@gmail.com>
2019-03-05 23:42:58 -0500

    add css to js with emotion (#2)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import { Provider } from "./SearchContext";
 import { petfinder } from "./config";
 import Results from "./Results";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+import NavBar from "./NavBar";
 import "./main.css";
 
 class App extends React.Component {
@@ -75,14 +76,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-          <Link to="/search-params">
-            <span aria-label="search" role="img">
-              🔍
-            </span>
-          </Link>
-        </header>
+        <NavBar />
         <Provider value={this.state}>
           <Router>
             <Results path="/" />
